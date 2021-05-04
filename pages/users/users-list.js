f031da135689a2f8272dcd4d639a9746ffc63b57
Tharanga <tharanga.thennakoon@axiatadigitallabs.com>
2021-05-05 02:03:39 +0530

    .

--- pages/users/users-list.js
+++ pages/users/users-list.js
@@ -1,8 +1,37 @@
 
-export default function Users() {
+function Users({ posts }) {
     return (
         <div>
             Users List
+            <ul>
+                {posts.map((post) => (
+                    <li>{post.title}</li>
+                ))}
+            </ul>
         </div>
     )
 }
+
+export async function getStaticProps() {
+    // Call an external API endpoint to get posts.
+    // You can use any data fetching library
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const posts = await res.json()
+
+    if (!posts) {
+        return {
+            notFound: true,
+        }
+    }
+
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+        props: {
+            posts,
+        },
+    }
+}
+
+
+export default Users
